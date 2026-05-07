033db9ff1d1aaf27f3f3bab2384ce410ed30e037
MANKARAN SINGH <mankaransingh@MacBookAir.lan>
2026-05-07 05:49:37 +0100

    Fix Azure port

--- server.js
+++ server.js
@@ -126,6 +126,8 @@ app.delete("/delete/:filename", async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
